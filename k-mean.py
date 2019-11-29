a7ecec149a261c58865c346dded6c3815ad994e3
mostafAanwar <mostafa.anwar12437@hotmail.com>
2019-11-29 15:09:13 +0200

    Initial commit

---
+++ k-mean.py
@@ -0,0 +1,11 @@
+import collections
+from idlelib.multicall import r
+from collections import Counter
+import itertools
+from itertools import chain
+from itertools import permutations
+from tkinter import *
+from itertools import *
+import imaplib
+import pandas as pd
+import numpy as np
