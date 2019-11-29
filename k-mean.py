b44cd6b95a1cd9ecdd8c41abff06ff0dd5cb79ef
mostafAanwar <mostafa.anwar12437@hotmail.com>
2019-11-29 17:43:05 +0200

    updated the file

--- k-mean.py
+++ k-mean.py
@@ -9,3 +9,15 @@ from itertools import *
 import imaplib
 import pandas as pd
 import numpy as np
+import xlrd
+import csv
+
+# The implementation
+# df = pd.read_excel(r'D:\CS\projects\k-mean\Absenteeism_at_work.xls')
+file = open("Absenteeism_at_work.csv")
+percentage = int(input("How many % of records you want to read?"))
+
+num_lines = len(file.readlines())
+records = int((percentage / 100) * num_lines)
+
+dataSet = pd.read_csv("Absenteeism_at_work.csv", sep=',', header=0, nrows=records).values
